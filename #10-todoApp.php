582e8c15dcf48ee7b84c630dae644f4616be67e8
yasir-rtx <yasir112358@gmail.com>
2024-06-20 11:28:16 +0700

    Update #10-todoApp.php : Clean Code

--- #10-todoApp.php
+++ #10-todoApp.php
@@ -1,58 +1,49 @@
 <?php
+    // Declare empty array of books
     $books = [];
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // functions
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Save Data
+    function saveData ($books) {
+        // Encode new data from array to json object
+        $readingList = json_encode($books, JSON_PRETTY_PRINT);  // JSON_PRETTY_PRINT => make indent in json file
+        
+        // Update json file with new data
+        file_put_contents("readingList.json", $readingList);
+        
+        // redirecting page to #10-todoApp.php
+        header('location:%2310-todoApp.php');
+    }
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     // cek file readingList.json sudah dibuat atau belum
     if (file_exists("readingList.json")) {
         $file = file_get_contents("readingList.json");
-        $books = json_decode($file, true);
+        $books = json_decode($file, true); // true => decode json object to array
     }
 
-    if (isset($_POST['books'])) {   // cek ada data baru
-        $data = $_POST["books"];    // mengambil data dari form
-        $books[] = [
-            'title' => $data,
-            'status' => 0
-        ];
-        $readingList = json_encode($books, JSON_PRETTY_PRINT);
-        file_put_contents("readingList.json", $readingList);
-
-        // directing to index.php to prevent double input
-        header('location:%2310-todoApp.php');
+    // Add new data
+    if (isset($_POST['save'])) {
+        // Assign title to $title
+        strlen($_POST['title'])==0 ? $title = "No Title" : $title = $_POST['title'];
+        // Assign new book to array
+        $books[] = ['title' => $title,'status' => 0];
+        saveData($books);
     }
 
     // Update status
     if (isset($_GET['status'])) {
         $books[$_GET['key']]['status'] = $_GET['status'];
-
-        $readingList = json_encode($books, JSON_PRETTY_PRINT);
-        file_put_contents("readingList.json", $readingList);
-
-        // redirect page
-        header('location:%2310-todoApp.php');
-
-        // Debug
-        // print_r($_GET['status']);
-        // echo "<br>";
-        // print_r($books[$_GET['key']]['status']);
+        saveData($books);               // save data
     }
 
     // Delete Book
     if (isset($_GET['delete'])) {
-        // delete data according to key
-        unset($books[$_GET['key']]);
-
-        // encode data to json
-        $readingList = json_encode($books, JSON_PRETTY_PRINT);
-        print_r ($readingList);
-        // // Update data
-        file_put_contents("readingList.json", $readingList);        
-        header('location:%2310-todoApp.php');
+        unset($books[$_GET['key']]);    // delete data according to key
+        saveData($books);               // save data
     }
-
-    // debug
-    echo "<br>";
-    echo "<br>";
-    print_r($books);
 ?>
 
 <!DOCTYPE html>
@@ -66,22 +57,27 @@
 
 <body>
     <h1>Reading List</h1>
+    
+    <!-- Form Input Data -->
     <form action="" method="POST">
-        <label for="books">Book : </label>
-        <input type="text" name="books" id="books">
-        <button type="submit">save</button>
+        <label for="title">Book : </label>
+        <input type="text" name="title" id="title">
+        <button type="submit" name="save">save</button>
     </form>
 
+    <!-- Books List -->
     <ul>
         <?php foreach ($books as $key => $val) : ?>
             <li>
+                <!-- Checkbox -->
                 <input 
                     type="checkbox" 
                     name="book" 
                     onclick="window.location.href='%2310-todoApp.php?status=<?php echo ($val['status'] == 1) ? 0 : 1 ?>&key=<?php echo $key; ?>'" 
                     <?php if ($val['status'] == 1) echo 'checked' ?>
                 >
-                <span><?php echo $key; ?></span>
+
+                <!-- Book title -->
                 <label for="<?php echo $val['title']; ?>">
                     <?php
                     if ($val['status'] == 1) {
@@ -91,6 +87,8 @@
                     }
                     ?>
                 </label>
+
+                <!-- Delete Button -->
                 <a href="%2310-todoApp.php?delete=1&key=<?php echo $key; ?>" onclick="return confirm('Delete Data?')">delete</a>
             </li>
         <?php endforeach; ?>
